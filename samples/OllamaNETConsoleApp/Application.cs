fa34ce81e86e42adf28be62825103f0918047db6
N1K0232 <ns.nicolasilvestri@libero.it>
2025-11-26 20:40:34 +0100

    add Console application sample

---
+++ samples/OllamaNETConsoleApp/Application.cs
@@ -0,0 +1,24 @@
+﻿using OllamaNET;
+using OllamaNET.Models;
+
+namespace OllamaNETConsoleApp;
+
+public class Application(IOllamaClient client)
+{
+    public async Task ExecuteAsync()
+    {
+        var conversationId = Guid.CreateVersion7();
+        string? message;
+
+        do
+        {
+            Console.Write("Ask me anything: ");
+            message = Console.ReadLine();
+
+            var response = await client.AskAsync(conversationId, message!);
+            Console.WriteLine(response.Message?.Content);
+            Console.WriteLine();
+        }
+        while (!string.IsNullOrWhiteSpace(message));
+    }
+}
---
+++ samples/OllamaNETConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OllamaNET.Extensions;
+using OllamaNETConsoleApp;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureServices)
+    .Build();
+
+var application = host.Services.GetRequiredService<Application>();
+await application.ExecuteAsync();
+
+void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+{
+    services.AddOllamaClient().WithDistributedCache();
+    services.AddSingleton<Application>();
+}
