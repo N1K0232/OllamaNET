d9e95002a3308b11f68d5f22515706ba205d55f8
N1K0232 <ns.nicolasilvestri@libero.it>
2025-11-26 21:32:53 +0100

    Add exception handling

---
+++ src/OllamaNET/Exceptions/OllamaClientException.cs
@@ -0,0 +1,10 @@
+﻿namespace OllamaNET.Exceptions;
+
+public class OllamaClientException(string? message, int statusCode, Exception? innerException) : Exception(message, innerException)
+{
+    public int StatusCode { get; } = statusCode;
+
+    public OllamaClientException(string message, int statusCode) : this(message, statusCode, null)
+    {
+    }
+}
