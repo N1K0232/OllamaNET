e135272c5c06b3dc64638c4793be4de122843ab0
N1K0232 <ns.nicolasilvestri@libero.it>
2025-12-03 22:19:53 +0100

    update sample

--- samples/OllamaNETConsoleApp/Application.cs
+++ samples/OllamaNETConsoleApp/Application.cs
@@ -6,19 +6,20 @@ public class Application(IOllamaClient client)
 {
     public async Task ExecuteAsync()
     {
-        var text = "Questo è un testo";
-        var embeddings = await client.CreateEmbeddingAsync(text);
+        //var text = "Questo è un testo";
+        //var embeddings = await client.CreateEmbeddingAsync(text);
 
-        Console.WriteLine(embeddings?.Embedding?.Length);
+        //Console.WriteLine(embeddings?.Embedding?.Length);
 
-        var setupMessage = """
-            You're an assistant that must help with software development tasks.
-            If the user asks anything else than software development related questions, you must politely refuse to answer.
-            If you don't know the answer, reply suggesting to refine the question.
-            Reply in the same language of the question.
-            """;
+        var conversationId = Guid.CreateVersion7();
+        Console.Write("Instruct the AI on his behaviour: (press enter to skip without instructions) ");
 
-        var conversationId = await client.SetupAsync(Guid.CreateVersion7(), setupMessage);
+        var setupMessage = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(setupMessage))
+        {
+            await client.SetupAsync(Guid.CreateVersion7(), setupMessage);
+        }
+
         string? message;
 
         do
@@ -33,7 +34,7 @@ public class Application(IOllamaClient client)
 
             await foreach (var response in responseStream)
             {
-                await Task.Delay(10);
+                await Task.Delay(5);
                 Console.Write(response.Message!.Content);
             }
 
